2da924f5097189ada779893dfc8a8ef0f4009df6
Jack Herrington <jherr@pobox.com>
2024-02-17 16:11:47 -0800

    RSCs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Suspense } from "react";
 import { getServerSession } from "next-auth";
 
 import { Separator } from "@/components/ui/separator";
 import Chat from "@/app/components/Chat";
+import PreviousChats from "@/app/components/PreviousChats";
 
 export default async function Home() {
   const session = await getServerSession();
@@ -12,6 +14,10 @@ export default async function Home() {
       {!session?.user?.email && <div>You need to log in to use this chat.</div>}
       {session?.user?.email && (
         <>
+          <Suspense fallback={<div>Loading Previous Chats</div>}>
+            <PreviousChats />
+          </Suspense>
+          <h4 className="mt-5 text-2xl font-bold">New Chat Session</h4>
           <Separator className="my-5" />
           <Chat />
         </>
